5ad134721da8dde2f7e6069d2bb112edf3b8ed3b
Piya-Boy <87295280+Piya-Boy@users.noreply.github.com>
2023-12-27 19:21:16 +0700

    Update main.js

--- HTML/Homework10/main.js
+++ HTML/Homework10/main.js
@@ -1,6 +1,24 @@
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function Counter({ sum, setSum, removeCounter }) {
+// function Counter({ sum, setSum, removeCounter }) {
+//   const updateCounter = (value) => {
+//     if (sum + value >= 0) {
+//       setSum(sum + value);
+//     }
+//   };
+
+//   return (
+//     <div className='counter'>
+//       <button onClick={() => updateCounter(-1)}> - </button>
+//       <h3>{sum}</h3>
+//       <button onClick={() => updateCounter(1)}> + </button>
+//       <button onClick={() => setSum(0)}> C </button>
+//       <button onClick={() => removeCounter()}> X </button>
+//     </div>
+//   );
+// }
+
+const Counter = ({ sum, setSum, removeCounter }) => {
   const updateCounter = (value) => {
     if (sum + value >= 0) {
       setSum(sum + value);
@@ -15,10 +33,26 @@ function Counter({ sum, setSum, removeCounter }) {
       <button onClick={() => setSum(0)}> C </button>
       <button onClick={() => removeCounter()}> X </button>
     </div>
-  );
+  )
 }
 
-function AddButton({ onClick }) {
+// function AddButton({ onClick }) {
+//   return (
+//     <div className='addbutton'>
+//       <button onClick={onClick}>Add</button>
+//     </div>
+//   );
+// }
+
+// function SumInfo({ sum }) {
+//   return (
+//     <div className='suminfo'>
+//       <h1>Sum = {sum}</h1>
+//     </div>
+//   );
+// }
+
+const AddButton = ({ onClick }) => {
   return (
     <div className='addbutton'>
       <button onClick={onClick}>Add</button>
@@ -26,7 +60,7 @@ function AddButton({ onClick }) {
   );
 }
 
-function SumInfo({ sum }) {
+const SumInfo = ({ sum }) => {
   return (
     <div className='suminfo'>
       <h1>Sum = {sum}</h1>
@@ -34,7 +68,47 @@ function SumInfo({ sum }) {
   );
 }
 
-function App() {
+
+// function App() {
+//   const [counters, setCounters] = React.useState([0]);
+//   const [sum, setSum] = React.useState(0);
+
+//   const addCounter = () => {
+//     setCounters([...counters, 0]);
+//   };
+
+//   // removeCounter
+//   function removeCounter(index) {
+//     const updatedCounters = [...counters];
+//     updatedCounters.splice(index, 1);
+//     setCounters(updatedCounters);
+//     setSum(updatedCounters.reduce((acc, curr) => acc + curr, 0));
+//   }
+
+//   return (
+//     <>
+//       <h1>Counter</h1>
+//       <AddButton onClick={addCounter} />
+
+//       <SumInfo sum={sum} />
+//       {counters.map((count, index) => (
+//         <Counter
+//           key={index}
+//           sum={count}
+//           setSum={(value) => {
+//             const updatedCounters = [...counters];
+//             updatedCounters[index] = value;
+//             setCounters(updatedCounters);
+//             setSum(updatedCounters.reduce((acc, curr) => acc + curr, 0));
+//           }}
+//           removeCounter={() => removeCounter(index)}
+//         />
+//       ))}
+//     </>
+//   );
+// }
+
+const App = () => {
   const [counters, setCounters] = React.useState([0]);
   const [sum, setSum] = React.useState(0);
 
@@ -42,14 +116,15 @@ function App() {
     setCounters([...counters, 0]);
   };
 
-  // removeCounter
-  function removeCounter(index) {
-    const updatedCounters = [...counters];
-    updatedCounters.splice(index, 1);
-    setCounters(updatedCounters);
-    setSum(updatedCounters.reduce((acc, curr) => acc + curr, 0));
-  }
 
+  
+const removeCounter = (index) => {
+  const updatedCounters = [...counters];
+  updatedCounters.splice(index, 1);
+  setCounters(updatedCounters);
+  setSum(updatedCounters.reduce((acc, curr) => acc + curr, 0));
+  }
+  
   return (
     <>
       <h1>Counter</h1>
@@ -57,7 +132,7 @@ function App() {
 
       <SumInfo sum={sum} />
       {counters.map((count, index) => (
-        <Counter
+        <Counter 
           key={index}
           sum={count}
           setSum={(value) => {
@@ -70,7 +145,7 @@ function App() {
         />
       ))}
     </>
-  );
+  )
 }
 
 root.render(<App />);
